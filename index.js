42bcbf52acfeb285f6b308a6f8cb7e96e6bc9991
sharifulislam565619 <sharifulislam565619@gmail.com>
2021-11-10 20:36:41 +0600

    order product send to db

--- index.js
+++ index.js
@@ -20,15 +20,12 @@ async function run() {
       await client.connect();
       const database = client.db("dream-bike");
       const productCollection = database.collection("products");
-
-      // const database2 = client.db("allOrder");
-      // const orders = database2.collection("orders");
+      const orderCollection = database.collection("orders");
 
       // insert one product
       app.post("/addProduct", async (req, res) => {
          const data = req.body;
          const doc = {
-
             name: data.name,
             description: data.description,
             price: data.price,
@@ -37,11 +34,85 @@ async function run() {
          const result = await productCollection.insertOne(doc);
          res.json(result)
          console.log(result)
+      })
+
+      // get all products 
+      app.get('/products', async (req, res) => {
+         const result = await productCollection.find({}).toArray()
+         res.json(result);
+      })
+
+      // // get all orders data
+      // app.get("/manageOrders", async (req, res) => {
+
+      //    const cursor = await orders.find({}).toArray();
+      //    res.send(cursor)
+
+      // })
+
+      // // get MyOrders
+      // app.get("/myOrders/:email", async (req, res) => {
+      //    const result = await orders.find({
+      //       email: req.params.email,
+      //    }).toArray();
+      //    res.send(result);
+      // });
+
+      // // update status
+      // app.put("/status/:id", async (req, res) => {
+
+      //    const id = req.params.id
+      //    const filter = { _id: ObjectId(id) };
+      //    const options = { upsert: true };
+      //    const updateDoc = {
+      //       $set: {
+      //          status: req.body.status
+      //       },
+      //    };
+      //    const result = await orders.updateOne(filter, updateDoc, options);
+      //    res.json(result)
 
 
+      // })
+
+      // // delete orders
+      // app.delete("/delete/:id", async (req, res) => {
+      //    const id = req.params.id;
+      //    const query = { _id: ObjectId(id) }
+      //    const result = await orders.deleteOne(query)
+      //    res.json(result)
+      //    console.log(result);
+      // })
+
+      // get single product
+      app.get('/product/:id', async (req, res) => {
+         const id = req.params.id
+         const query = { _id: ObjectId(id) };
+         const result = await productCollection.findOne(query);
+         res.json(result);
       })
 
 
+
+      // Order product
+      app.post("/order", async (req, res) => {
+         const data = req.body;
+         const doc = {
+            order_id: data.order_id,
+            name: data.name,
+            emailAddress: data.emailAddress,
+            phone: data.phone,
+            status: data.status,
+            address: data.address,
+            email: data.email,
+            img: data.img,
+            price: data.price,
+            orderName: data.orderName,
+         }
+         const result = await orderCollection.insertOne(doc);
+         res.json(result)
+         console.log(result);
+      })
 
 
    } finally {
